fbc0c59136acfb6bc2fb28d9d5a46d35bfae6bfe
Himanshu Yadav <himanshu.skb.oo7@gmail.com>
2023-05-01 02:05:57 +0530

    updating to latest flask version

--- app.py
+++ app.py
@@ -1,42 +1,41 @@
 import os
 from flask import Flask
-from flaskext.mysql import MySQL      # For newer versions of flask-mysql 
-# from flask.ext.mysql import MySQL   # For older versions of flask-mysql
+import mysql.connector
+
 app = Flask(__name__)
 
-mysql = MySQL()
+# configure db
+mysql_database_host = 'MYSQL_DATABASE_HOST' in os.environ and os.environ['MYSQL_DATABASE_HOST'] or 'localhost'
+db = mysql.connector.connect(
+    host=mysql_database_host,
+    user="db_user",
+    password="Passw0rd",
+    database="employee_db"
+)
 
-mysql_database_host = 'MYSQL_DATABASE_HOST' in os.environ and os.environ['MYSQL_DATABASE_HOST'] or  'localhost'
 
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'db_user'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Passw0rd'
-app.config['MYSQL_DATABASE_DB'] = 'employee_db'
-app.config['MYSQL_DATABASE_HOST'] = mysql_database_host
-mysql.init_app(app)
-
-conn = mysql.connect()
-
-cursor = conn.cursor()
-
-@app.route("/")
+@app.route("/", methods=['GET'])
 def main():
     return "Welcome!"
 
-@app.route('/how are you')
+@app.route('/how are you', methods=['GET'])
 def hello():
     return 'I am good, how about you?'
 
-@app.route('/read from database')
-def read():
+@app.route('/read from database', methods=['GET'])
+def get_employees():
+    # Fetch data from employees table
+    cursor = db.cursor()
     cursor.execute("SELECT * FROM employees")
-    row = cursor.fetchone()
-    result = []
-    while row is not None:
-      result.append(row[0])
-      row = cursor.fetchone()
+    results = cursor.fetchall()
 
-    return ",".join(result)
+    # Join all the data into a string
+    employee_data = ""
+    for row in results:
+        employee_data += f"{row[1]} {row[2]} ({row[3]}) - Hired on {row[5]} with a salary of ${row[6]}\n"
+
+    # Return the joined string
+    return employee_data
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=80)
